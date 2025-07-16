823b3ee3a9256db535b0e7f6d054d0e59e41ec49
Miguel Malek <mimalek@2-G-5.42heilbronn.de>
2025-07-16 17:41:09 +0200

    BUILD(parse.c parse.h) -> Begin with the logic for the validation and parsing

--- inc/parse.h
+++ inc/parse.h
@@ -1,6 +1,6 @@
 #ifndef PARSE_H
 # define PARSE_H
 
-
+int	ft_validate_parse_file(t_cub3d *data, int fd);
 
 #endif
---
+++ src/parse.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 17:13:21 by mimalek           #+#    #+#             */
+/*   Updated: 2025/07/16 17:37:26 by mimalek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int	ft_validate_parse_file(t_cub3d *data, int fd)
+{
+	char	*line = NULL;
+	int		i = 0;
+	(void)data;
+	while (get_next_line(fd, &line) != NULL)
+		ft_printf("%d: %s\n", i++, line);
+	return (0);
+}
