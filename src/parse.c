e4ad0011a652fbca943bec746a41ef4feb7c2023
Miguel Malek <mimalek@2-G-5.42heilbronn.de>
2025-07-16 19:06:04 +0200

    BUILD(parse.c) -> Reading trough the file

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 17:13:21 by mimalek           #+#    #+#             */
-/*   Updated: 2025/07/16 17:37:26 by mimalek          ###   ########.fr       */
+/*   Updated: 2025/07/16 17:44:17 by mimalek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 int	ft_validate_parse_file(t_cub3d *data, int fd)
 {
-	char	*line = NULL;
+	char	*line;
 	int		i = 0;
+
 	(void)data;
+	line = NULL;
 	while (get_next_line(fd, &line) != NULL)
 		ft_printf("%d: %s\n", i++, line);
 	return (0);
