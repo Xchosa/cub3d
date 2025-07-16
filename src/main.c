05134283cd48510953a4ebdac5d0e897d0e8fd41
Miguel Malek <mimalek@1-E-23.42heilbronn.de>
2025-07-16 15:47:48 +0200

    FEAT(main.c) -> As well some argument validation

--- src/main.c
+++ src/main.c
@@ -6,15 +6,33 @@
 /*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 14:14:43 by mimalek           #+#    #+#             */
-/*   Updated: 2025/07/16 14:59:00 by mimalek          ###   ########.fr       */
+/*   Updated: 2025/07/16 15:44:28 by mimalek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static int	argument_check(int argc, char **argv);
+
 int	main(int argc, char **argv)
 {
-	(void)argv;
+	int	fd;
+
+	fd = argument_check(argc, argv);
+	
+	return (close(fd), 0);
+}
+
+static int	argument_check(int argc, char **argv)
+{
+	int	fd;
+
 	if (argc != 2)
-		return (ft_error(ARGUMENT_AMOUNT), ARGUMENT_AMOUNT);
+		return (ft_error(ARGUMENT_AMOUNT), -1);
+	if (!ft_strnstr(argv[1], ".cub", ft_strlen(argv[1])))
+		return (ft_error(WRONG_FILE_TYPE), -1);
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		return (ft_error(OPEN_FAILED), -1);
+	return (fd);
 }
