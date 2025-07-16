5a495c975bd6d071ee8f73dfa01bfa477ce0e2a2
Miguel Malek <mimalek@1-E-23.42heilbronn.de>
2025-07-16 15:05:15 +0200

    BUILD(cub3d.h) -> Added a Cub3d header to summerize all headers

---
+++ inc/cub3d.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 14:11:24 by mimalek           #+#    #+#             */
+/*   Updated: 2025/07/16 14:57:43 by mimalek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+#define CUB3D_H
+
+# include <stdbool.h>
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# include "libft.h"
+
+#include "init.h"
+#include "error.h"
+
+#endif
