49b098d9a3859fb94475a5dfca12de22ddb206d0
Miguel Malek <mimalek@1-E-23.42heilbronn.de>
2025-07-16 15:46:08 +0200

    BUILD(error.h) -> Added some error types

--- inc/error.h
+++ inc/error.h
@@ -6,13 +6,15 @@
 /*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 14:16:33 by mimalek           #+#    #+#             */
-/*   Updated: 2025/07/16 14:57:24 by mimalek          ###   ########.fr       */
+/*   Updated: 2025/07/16 15:39:30 by mimalek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 typedef	enum	e_error
 {
 	ARGUMENT_AMOUNT = 1,
+	WRONG_FILE_TYPE = 2,
+	OPEN_FAILED		= 3,
 }				t_error;
 
 void	ft_error( int error_code );
