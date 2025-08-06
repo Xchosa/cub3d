cd1bf9155b9981fc8ca7c57025b27b8bf2c7ab9c
Paul Overbeck <paul.overbeck95@gmail.com>
2025-08-06 10:13:33 +0200

    draw _minimap with objects

--- src/init/draw_minimap.c
+++ src/init/draw_minimap.c
@@ -27,7 +27,7 @@ void	draw_minimap(t_cub3d *cub3d)
 	
 	minimap_x = cub3d->window_width - cub3d->minimap_img_width - 10;
 	minimap_y = 10;
-	draw_minimap_background(cub3d, minimap_x ,minimap_y);
+	draw_minimap_background(cub3d, minimap_x, minimap_y);
 	
 	draw_minimap_cells(cub3d, minimap_x, minimap_y, );
 }
@@ -57,7 +57,7 @@ void	draw_minimap_background(t_cub3d *cub3d, int start_x, int start_y)
 // map_height, map_width und sqaure size in eine static function auslagern 
 // und so in der draw square callen 
 
-void	draw_minimap_cells(t_cub3d *cub3d, int map_height, int map_width, int minimap_x, int minimap_y)
+void	draw_minimap_cells(t_cub3d *cub3d int minimap_x, int minimap_y)
 {
 	int square_size;
 	int map_height;
@@ -65,6 +65,8 @@ void	draw_minimap_cells(t_cub3d *cub3d, int map_height, int map_width, int minim
 	int x;
 	int y;
 	uint32_t cell_color;
+	int pixel_x;
+	int pixel_y;
 
 	// map_height = get_map_height(cub3d->map);
 	// map_width = get_map_width(cub3d->map);
@@ -72,18 +74,43 @@ void	draw_minimap_cells(t_cub3d *cub3d, int map_height, int map_width, int minim
 
 	(void)minimap_x;
 	(void)minimap_y;
-	// change x to y 
-	x = 0;
-	while (x < map_width )
+	
+	y = 0;
+	while (y < get_object()->map_height)
 	{
-		y = 0;
-		while (y < map_height)
+		x = 0;
+		while (x < get_object()-> map_width)
 		{
-			cell_color = get_cell_color(cub3d->map[x][y]);
-			draw_square(cub3d, cell_color, x, y, square_size);
-			y++;
+			cell_color = get_cell_color(cub3d->map[y][x]);
+			pixel_x = minimap_x + (x * get_object()-> square_size);
+			pixel_y = minimap_y +y (y * get_object()-> square_size)
+			draw_square(cub3d, cell_color, pixel_x, pixel_y);
+			x++;
 		}
-		x++;
+		y++;
 	}
 }
-	
+
+void	draw_square(t_cub3d *cub3d, uint32_t color, int start_x,
+		int start_y)
+{
+	int	x,
+	int y;
+
+	y = 0;
+	while(y < get_object()->square_size)
+	{
+		x = 0;
+		while(x < get_object()->square_size)
+		{
+			if (start_x + x >= 0 && start_x + x < (int)cub3d->window_width &&
+                start_y + y >= 0 && start_y + y < (int)cub3d->window_height)
+			{
+				mlx_put_pixel(cub3d->img, start_x + x, start_y + y, color)
+			}
+			x++;
+		}
+		y++;
+	}
+}
+
