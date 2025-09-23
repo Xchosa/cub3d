59867ae38a2aa831de243b868752b1bfe6d59330
Miguel Malek <mimalek@3-H-6.42heilbronn.de>
2025-09-23 14:22:10 +0200

    updated

--- src/game_loop/render_walls.c
+++ src/game_loop/render_walls.c
@@ -12,14 +12,38 @@
 // */
 
 
-mlx_image_t load_texture(t_cub3d *cub3d)
+mlx_texture_t *load_wall_texture(t_cub3d *cub3d, t_ray *ray)
 {
-    mlx_image_t	wall_texture;
-    
+    mlx_texture_t	*wall_texture;
 
-    wall_texture = cub3d->graphics->north.img;
+	if (ray->side == 0 && ray->delta_x > 0) // vertical wall, ray points to positive x direction (east)
+		wall_texture = cub3d->graphics->east.texture;
+	else if (ray->side == 0 && ray->delta_x < 0) // vertical wall, ray points to negative x direction (west)
+		wall_texture = cub3d->graphics->west.texture;
+	else if (ray->side == 1 && ray->delta_y > 0) // horizontal wall, ray points to positive y direction (south)
+		wall_texture = cub3d->graphics->south.texture;
+	else // horizontal wall, ray points to negative y direction (north)
+    	wall_texture = cub3d->graphics->north.texture;
+	
     return(wall_texture);
 }
+
+int return_x_cor_texture(int wall_x, t_cub3d *cub3d, mlx_texture_t *wall_texture)
+{
+	if( wall_texture == cub3d->graphics->north.texture)
+		return ((int)(wall_x *cub3d->graphics->north.texture->width));
+	else  if( wall_texture == cub3d->graphics->east.texture)
+		return ((int)(wall_x *cub3d->graphics->east.texture->width));
+	else if( wall_texture == cub3d->graphics->west.texture)
+		return ((int)(wall_x *cub3d->graphics->west.texture->width));
+	else
+		return ((int)(wall_x *cub3d->graphics->south.texture->width));
+
+	}
+
+
+
+
 
 void draw_column( t_cub3d *cub3d, t_ray *ray, int x)
 {
@@ -28,7 +52,11 @@ void draw_column( t_cub3d *cub3d, t_ray *ray, int x)
     double      wall_x;
     int         y;
     int         text_x;
-    mlx_texture_t	wall_texture;
+    mlx_texture_t	*wall_texture;
+	uint32_t	*texture_pix;
+	double		step;
+	double		texture_position;
+	uint32_t	color;
 
     ceiling_color = (cub3d->graphics->ceiling_colour[0] << 24) |
                     (cub3d->graphics->ceiling_colour[1] << 16) |
@@ -42,17 +70,24 @@ void draw_column( t_cub3d *cub3d, t_ray *ray, int x)
 
     // calculate wall position of ray hit (0.0 to 1.0)
     if (ray->side == 0 )// vertical Wall
-        wall_x = ray->ray_y + ray->wall_dist + ray->delta_y;
+        wall_x = ray->ray_y + ray->wall_dist * ray->delta_y;
     else
-    // horizontl
-        wall_x = ray->ray_x + ray->wall_dist + ray->delta_x;
+    // horizontal
+        wall_x = ray->ray_x + ray->wall_dist * ray->delta_x;
 
+
+
+	
     wall_x = wall_x - floor(wall_x); // rundet immer ab -1,3 = -2 / 
     // get decimalstellen/ franction part
 
-    wall_texture = load_texture;
+    wall_texture = load_wall_texture(cub3d, ray);
+	// text_x = return_x_cor_texture(wall_x, cub3d, wall_texture);
+	texture_pix = (uint32_t*)wall_texture->pixels;
+	text_x = (int)(wall_x * wall_texture->width);
+
     // texture x cordinate
-    text_x = (int)(wall_x *cub3d->graphics->north.texture->width);
+	
     
     // check for horizontall flip 
     // ray looks east  pos x direction && ray points north (neg y direction )
@@ -63,7 +98,8 @@ void draw_column( t_cub3d *cub3d, t_ray *ray, int x)
 
     // x achse durch ray , x wert 3, 5 an der wand 
     // brechnen wo der ray an y
-
+	step = (double)wall_texture->height / ray->line_height;
+	texture_position = (ray->draw_start - cub3d->window_height / 2 + ray->line_height / 2) * step;
     // ceiling
     y = 0;
     while (y < ray->draw_start)
@@ -71,11 +107,13 @@ void draw_column( t_cub3d *cub3d, t_ray *ray, int x)
 		mlx_put_pixel(cub3d->img, x, y, ceiling_color);
 		y++;
 	}
-    // while (y <= ray->draw_end)
-	// {
-	// 	mlx_put_pixel(cub3d->img, x, y, wall_color);
-	// 	y++;
-	// }
+    while (y <= ray->draw_end)
+	{
+		color = texture_pix[(int)texture_position * wall_texture->width + text_x];
+		mlx_put_pixel(cub3d->img, x, y, color);
+		texture_position += step;
+		y++;
+	}
 	while (y < (int)cub3d->window_height)
 	{
 		mlx_put_pixel(cub3d->img, x, y, floor_color);
